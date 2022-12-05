b90360b114bdcd8c7d35cc4cd1fba2c9d3e79c33
Lonnie Souder II <lonnie2@souderfamily.net>
2022-12-05 15:36:33 -0500

    docs: added docs to RotationHandle.cs

--- Assets/RotationHandle.cs
+++ Assets/RotationHandle.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+* <summary>
+* UI Handle that rotates and object about the y axis.
+* Note: User interaction still happens along the z axis like all other <c>Handle</c>s
+*       Since this causes the z axis to rotate, the appropriate user action
+*       is to trace a circle around the y axis
+* </summary>
+*/
 public class RotationHandle : Handle
 {
     protected override void DoTransform(float magnitude)
@@ -9,11 +17,18 @@ public class RotationHandle : Handle
         transform.parent.transform.RotateAround(transform.parent.position, transform.up, magnitude);
     }
 
-    void Update() {
+    void Update()
+    {
         MaintainScale();
     }
 
-    private void MaintainScale() {
+    /**
+    * <summary>
+    * Modifies scale to ensure this element always surrounds the subject
+    * </summary>
+    */
+    private void MaintainScale()
+    {
         // Assumes ring scale has not been altered, so ring minor radius = 1.0f
         transform.localScale = GetMaxMeshDimension() * Vector3.one;
     }
